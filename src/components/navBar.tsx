47ca960045382be35cc5ecb60e2810107bf8581a
jackiemalley11 <jackiemalley11@gmail.com>
2025-02-25 08:43:10 -0400

    Updated NavBar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { useState } from "react";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,64 +9,65 @@ export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div className="relative bg-bgBlue">
-      {/* Flower Image */}
-      <div className="fixed top-0 left-0 z-10">
-        <img src="/images/MenuBarFlower.png" alt="Menu Bar Flower" className="w-1/3" />
-      </div>
-  
-      {/* Menu Bar */}
-      <div className="fixed top-0 right-0 z-20 rounded-lg flex justify-end items-center sm:w-auto sm:left-1/4 px-4 py-2">
+    <div className="fixed top-0 left-0 right-0 w-full bg-bgBlue z-50">
+      {/* Container for the entire navbar with edge-to-edge layout */}
+      <div className="flex justify-between items-center py-4 px-2">
+        {/* Logo/Brand section - pushed to far left */}
+        <div className="flex items-center ml-4">
+          <img src="/images/menuFlower.png" alt="Menu Bar Flower" className="w-12" />
+          <span className="gradient-text font-bold ml-4">JACKIE MALLEY</span>
+        </div>
+
         {/* Mobile Menu Toggle */}
         <button
-          className="block sm:hidden absolute right-4 top-4 text-3xl z-50 text-white"
+          className="block sm:hidden text-3xl text-white mr-2"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
           <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
         </button>
-  
-        {/* Mobile Menu Links */}
-        {isMenuOpen && (
-          <div className="sm:hidden bg-bgBlue text-white py-4">
-            <div className="flex flex-col items-center space-y-4">
-              <Link href="/projects" className="hover:text-hoverBlue">
-                PROJECTS
-              </Link>
-              <Link href="/experience" className="hover:text-hoverPurple">
-                EXPERIENCE
-              </Link>
-              <Link href="/aboutMe" className="hover:text-hoverPink">
-                ABOUT ME
-              </Link>
-              <Link href="/contactMe" className="hover:text-hoverYellow">
-                CONTACT
-              </Link>
-              <Link href="/" className="hover:text-hoverBlue">
-                <FontAwesomeIcon icon={faHouse} />
-              </Link>
-            </div>
-          </div>
-        )}
-  
-        {/* Desktop Menu Links */}
-        <div className="hidden sm:flex w-auto justify-end gap-4 mt-3 bg-transparent">
-          <Link href="/projects" className="hover:text-hoverBlue">
+
+        {/* Desktop Menu Links - pushed to far right */}
+        <div className="hidden sm:flex items-center ml-4 mr-2">
+          <Link href="/projects" className="hover:text-hoverBlue ml-6 px-4">
             PROJECTS
           </Link>
-          <Link href="/experience" className="hover:text-hoverPurple">
+          <Link href="/experience" className="hover:text-hoverPurple px-4">
             EXPERIENCE
           </Link>
-          <Link href="/aboutMe" className="hover:text-hoverPink">
+          <Link href="/aboutMe" className="hover:text-hoverPink px-4 whitespace-nowrap">
             ABOUT ME
           </Link>
-          <Link href="/contactMe" className="hover:text-hoverYellow">
+          <Link href="/contactMe" className="hover:text-hoverYellow px-4">
             CONTACT
           </Link>
-          <Link href="/" className="hover:text-hoverBlue">
+          <Link href="/" className="hover:text-hoverBlue px-4">
             <FontAwesomeIcon icon={faHouse} />
           </Link>
         </div>
       </div>
+
+      {/* Mobile Menu - Full-width dropdown */}
+      {isMenuOpen && (
+        <div className="sm:hidden bg-bgBlue text-white py-4 w-full">
+          <div className="flex flex-col items-center space-y-4">
+            <Link href="/projects" className="hover:text-hoverBlue">
+              PROJECTS
+            </Link>
+            <Link href="/experience" className="hover:text-hoverPurple">
+              EXPERIENCE
+            </Link>
+            <Link href="/aboutMe" className="hover:text-hoverPink whitespace-nowrap">
+              ABOUT ME
+            </Link>
+            <Link href="/contactMe" className="hover:text-hoverYellow">
+              CONTACT
+            </Link>
+            <Link href="/" className="hover:text-hoverBlue">
+              <FontAwesomeIcon icon={faHouse} />
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
